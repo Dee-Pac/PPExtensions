ce0cfc100d7bd99e04c47ab6e1bb7c9349c6194d
Dee-Pac <mcdeepak@gmail.com>
2018-08-24 16:13:17 -0700

    [#8] Add travis CI integration

--- build/ci.sh
+++ build/ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export HOME=$(pwd)
-export BUILD_DIR=${HOME}/build
-export WORK_DIR=${HOME}/.tmp
+export REPO_HOME=$(pwd)
+export BUILD_DIR=${REPO_HOME}/build
+export WORK_DIR=${REPO_HOME}/.tmp
 
 source ${BUILD_DIR}/functions.sh
 source ${BUILD_DIR}/env.sh
@@ -11,9 +11,29 @@ export args=$*
 
 write_log "Arguments --> ${args}"
 
+write_log "################### Cleanup #####################
+
+run_cmd "rm -rf ${WORK_DIR}"
+run_cmd "mkdir ${WORK_DIR}"
+
+write_log "#################### Install Requisite Packages ###############
+
+write_log " ----------- Install Tableau SDK ------------- "
+
+run_cmd "cd ${WORK_DIR}"
+run_cmd "wget ${TABLEAU_URL}"
+run_cmd "tar -xvf ${TABLEAU_TAR_BALL}"
+run_cmd "export TABLEAU_DIR=$(ls | grep Tableau | grep -v gz)"
+run_cmd "python ${TABLEAU_DIR}/setup.py install"
+run_cmd "cd .."
+
 write_log "####################  Install PPExtensions ####################"
 
 run_cmd "pip install ppextensions"
+
+write_log "################### Final Cleanup #########################"
+
+rm -rf ${WORK_DIR}
 
 write_log "########################  BUILD SUCCESS ###############################"
 
